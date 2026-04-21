e08cfcfc5b92cebd4d102e553c8a4c1a58b6f22a
cattrang69 <davissle353@gmail.com>
2026-04-21 16:39:38 -0400

    finish HelloWorld

--- HelloWorld.jsx
+++ HelloWorld.jsx
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
 
 class HelloWorld extends Component {
-    
+    render() {
+        return (
+            <div className="task-card">
+                <h1>Hello World!</h1>
+                <p>My first React component is rendered.</p>
+            </div>
+        );
+    }
 }
+
+
+export default HelloWorld;
